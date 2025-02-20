2e30c3eba0667edb3dc889808c03ca08a472425a
Zamanat <mali29april@gmail.>
2025-02-20 17:28:50 +0500

    window fixed

--- src/components/Ads/Leaderboard.tsx
+++ src/components/Ads/Leaderboard.tsx
@@ -4,41 +4,39 @@ import React, { useState, useEffect } from "react";
 import { Bling as GPT } from "react-gpt";
 
 const Leaderboard = () => {
-  const [windowSize, setWindowSize] = useState({
-    width: window.innerWidth,
-    height: window.innerHeight
-  });
+  const [windowSize, setWindowSize] = useState<{ width: number; height: number } | null>(null);
 
   useEffect(() => {
-    const handleResize = () => {
+    if (typeof window !== "undefined") {
       setWindowSize({
         width: window.innerWidth,
-        height: window.innerHeight
+        height: window.innerHeight,
       });
-    };
 
-    window.addEventListener("resize", handleResize);
-    return () => window.removeEventListener("resize", handleResize);
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+
+      window.addEventListener("resize", handleResize);
+      return () => window.removeEventListener("resize", handleResize);
+    }
   }, []);
 
   return (
     <div className="mt-5">
-      {windowSize.width > 768 ? (
-        <>
-        {/* <h2>Desktop</h2> */}
+      {windowSize && windowSize.width > 768 ? (
         <GPT
           adUnitPath="/67551462/A-Sports-RoS"
           slotSize={[[970, 250], [750, 300], [950, 90], [728, 90]]}
         />
-        </>
       ) : (
-        <>
-        {/* <h2>Mobile</h2> */}
         <GPT
           adUnitPath="/67551462/A-Sports-RoS"
           slotSize={[[320, 100], [320, 50]]}
         />
-        </>
       )}
     </div>
   );
