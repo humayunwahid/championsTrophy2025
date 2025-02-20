e9bae004ea4948c35da58cfa814c60cdd41f7990
humayun <humayunwahid70@gmail.com>
2025-02-20 11:48:42 +0500

    all basic things are done

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-      ignoreDuringBuilds: true, // Ignores ESLint errors during build
-    },
-    typescript: {
-      ignoreBuildErrors: true, // Ignores TypeScript errors during build
-    },
-  };
+    };
   
   export default nextConfig;
   
