0618366ce9f1e5271b5a1c3cb108f0e5e96bd4fd
Zamanat <mali29april@gmail.>
2025-02-24 13:48:29 +0500

    keyword changed on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,7 @@ import React from "react";
 export const metadata: Metadata = {
 	title: "Champions Trophy 2025 Matches, News, Results and Teams",
 	description: "Keep abreast of all the action from the Champions Trophy 2025! Get match schedules, latest news, live scores, team details, and expert opinions.",
-	keywords: "Champions Trophy 2025, Champions Trophy 2025 Points Table Pakistan, Champions Trophy 2025 Schedule, Champions Trophy 2025 Schedule Points Table pdf, ICC Champions Trophy 2025 rankings, Champions Trophy 2025 net run rate, Champions Trophy 2025 team standings, Champions Trophy 2025 match results",
+	keywords: "Champions Trophy, Champions Trophy 2025, Champions Trophy 2025 Points Table Pakistan, Champions Trophy 2025 Schedule, Champions Trophy 2025 Schedule Points Table pdf, ICC Champions Trophy 2025 rankings, Champions Trophy 2025 net run rate, Champions Trophy 2025 team standings, Champions Trophy 2025 match results",
 };
 
 const index = () => {
